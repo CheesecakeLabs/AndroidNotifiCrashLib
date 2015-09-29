6ac5405456a554e1f151eab476f845b6c85cff11
Natan Grando <natangrando@Natans-MacBook-Pro.local>
2015-09-29 15:47:59 -0300

    Added example passing the extra argument

--- crash-app/src/main/java/io/ckl/crash/app/CrashApplication.java
+++ crash-app/src/main/java/io/ckl/crash/app/CrashApplication.java
@@ -2,12 +2,19 @@ package io.ckl.crash.app;
 
 import android.app.Application;
 
+import java.util.HashMap;
+
 import io.ckl.notificrash.NotifiCrash;
+import io.ckl.notificrash.NotifiCrashArguments;
 
-public class CrashApplication extends Application {
+public class CrashApplication extends Application implements NotifiCrashArguments {
 
+//    private static final String NOTIFICRASH_SERIAL_NUMBER = "PUT_YOUR_NOTIFICRASH_SERIAL_HERE";
+    private static final String NOTIFICRASH_SERIAL_NUMBER = "30a8c031b61328738fe313fd678bc8";
 
-    private static final String NOTIFICRASH_SERIAL_NUMBER = "PUT_YOUR_NOTIFICRASH_SERIAL_HERE";
+    private String getUser() {
+        return "MyUser";
+    }
 
     @Override
     public void onCreate() {
@@ -18,6 +25,17 @@ public class CrashApplication extends Application {
         // Initializing NotifiCrash library
         NotifiCrash.init(this, NOTIFICRASH_SERIAL_NUMBER);
 
+        // Add an extra argument to NotifiCrash log
+        NotifiCrash.getInstance().addExtra("MyKey", "MyValue");
+
     }
 
+    // Method that retrieve the extra arguments when an app crashes
+    @Override
+    public HashMap<String, String> getExtraArguments() {
+        HashMap<String, String> extraArguments = new HashMap<>();
+        extraArguments.put("User", getUser());
+        extraArguments.put("Data", "MyData");
+        return extraArguments;
+    }
 }
